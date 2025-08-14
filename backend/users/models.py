001381e55a1e68dd24dc0c48639483a8a94986ee
Sergey Shagin <sergey.n.shagin@gmail.com>
2025-08-14 16:38:13 +0300

    Settings updated, model user added

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-# Create your models here.
+MAX_NAME_LENGTH = 150
+
+
+class FoodgramUser(AbstractUser):
+    """Модель пользователя с аватаром"""
+
+    username = models.CharField(
+        max_length=MAX_NAME_LENGTH,
+        unique=True,
+        validators=[UnicodeUsernameValidator(),],
+    )
+    email = models.EmailField(
+        "Email",
+        unique=True,
+    )
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        null=True,
+        default=None
+    )
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return (
+            f"Пользователь {self.username} "
+            f"(Роль: {self.role}, Email: {self.email})"
+        )
+
+
+User = get_user_model()
---
+++ backend/users/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework.serializers import (
+    ModelSerializer,
+)
+
+from users.models import User
+
+
+class UserSerializer(ModelSerializer):
+    """
+    Сериализатор по пользователям
+    """
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "bio",
+            "role",
+        )
